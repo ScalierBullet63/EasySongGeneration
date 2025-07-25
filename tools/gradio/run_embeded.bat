f08cebb4b88cefa37858e6ede952556b0c6e4497
ScalierBullet63 <199808687+ScalierBullet63@users.noreply.github.com>
2025-07-25 18:55:07 +0200

    Update .gitignore and requirements files; add run_embeded.bat for Gradio app with python_embeded

---
+++ tools/gradio/run_embeded.bat
@@ -0,0 +1,8 @@
+@echo off
+set USER=root
+set PYTHONDONTWRITEBYTECODE=1
+set TRANSFORMERS_CACHE=%cd%\third_party\hub
+set PYTHONPATH=%cd%\codeclm\tokenizer\;%cd%;%cd%\codeclm\tokenizer\Flow1dVAE\;%cd%\codeclm\tokenizer\;tools\gradio;%PYTHONPATH%
+
+set CKPT_PATH=%1
+%cd%\python_embeded\python.exe tools\gradio\app.py %CKPT_PATH%
